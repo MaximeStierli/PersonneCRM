39bc75de3cad6996b61e9883a50551558a05c7e4
Maxime Stierli <maxime.stierli@he-arc.ch>
2015-11-13 14:44:14 +0100

    Commit DAO et Model

--- CRUDPersonnesServletsMLogin/src/java/DAO/CommentaireDAO.java
+++ CRUDPersonnesServletsMLogin/src/java/DAO/CommentaireDAO.java
@@ -23,59 +23,12 @@ import oracle.jdbc.OracleTypes;
 public class CommentaireDAO {
     public CommentaireDAO(){};
 
-    public Vector<Personne>  select(Commentaire comm) {
+    public Vector<Commentaire>  select(Commentaire comm) {
         Connection conn = DBDataSource.getJDBCConnection();
         Statement stmt = null;
         ResultSet rs = null;
-        Vector<Personne> resultList=new Vector();
-        try {
-            String query=null,sn=null,snom=null,sprenom=null,sadr=null,sville=null;
-            boolean onedone=false;
-            query= "select * from Commentaire";
-       
-          //tester si on a des critères
-           if(comm.getId()!=null)sn=" numero="+comm.getId();
-           if(comm.getNom()!=null) snom=" NOM like '%" +comm.getNom()+"%' ";
-           if(p.getPrenom()!=null) sprenom=" PRENOM like '%" +comm.getPrenom()+"%' ";
-           if(p.getAdresse()!=null) sadr=" ADRESSE like '%" +comm.getAdresse()+"%' ";
-           if(p.getVille()!=null) sville=" VILLE like '%" +comm.getVille()+"%' ";
-           //si critères, contruire la clause where
-           if (sn!=null || snom!=null || sprenom!=null || sadr!=null || sville!=null) query=query.concat(" WHERE ");
-           //construction de la clause where
-           if(sn!=null){query=query.concat(sn);onedone=true;}
-           if(snom!=null){if (onedone)query=query.concat(" AND "); query=query.concat(snom); onedone=true;}
-           if(sprenom!=null){if (onedone)query=query.concat(" AND "); query=query.concat(sprenom); onedone=true;}
-           if(sadr!=null){if (onedone)query=query.concat(" AND "); query=query.concat(sadr); onedone=true;}
-           if(sville!=null){if (onedone)query=query.concat(" AND "); query=query.concat(sville); onedone=true;}
-
-           System.out.println(query);
-           stmt = conn.createStatement(); //create a statement
-           rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-                Long n = rs.getLong("NUMERO");
-                String nom = rs.getString("NOM");
-                String prenom = rs.getString("PRENOM");
-                String adresse = rs.getString("ADRESSE");
-                String ville = rs.getString("VILLE");
-                Personne pers=new Personne();
-                pers.setId(n);pers.setNom(nom);pers.setPrenom(prenom);pers.setAdresse(adresse);pers.setVille(ville);
-                resultList.add(pers);
-                System.out.println(n + "\t" + nom + "\t" + prenom + "\t" + adresse + "\t" + ville);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                rs.close();
-                stmt.close();
-                conn.close();
-                 return resultList;
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
+        Vector<Commentaire> resultList=new Vector();
+        return resultList;
        
     }
 
---
+++ CRUDPersonnesServletsMLogin/src/java/Model/Users.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Maxime Stierli <maxime.stierli@he-arc.ch>
+ */
+public class Users {
+    private Long id=null;
+    private String username;
+    private String pwd;
+    private String photo;
+    
+    public Users(){
+        
+    }
+}
