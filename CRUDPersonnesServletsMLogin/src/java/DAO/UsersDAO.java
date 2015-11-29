4e90f4b4a7cd04c83f042897d2c4d6111b60d9ae
yasmine mabrouk <yasmine.mabrouk@he-arc.ch>
2015-11-29 20:02:27 +0100

    taitement bon est fini

--- CRUDPersonnesServletsMLogin/src/java/DAO/UsersDAO.java
+++ CRUDPersonnesServletsMLogin/src/java/DAO/UsersDAO.java
@@ -22,22 +22,25 @@ import oracle.jdbc.OracleTypes;
  * @author Maxime Stierli <maxime.stierli@he-arc.ch>
  */
 public class UsersDAO {
-    public UsersDAO(){};
+
+    public UsersDAO() {
+    }
+
+    ;
     
         public ArrayList<Users> selectAll() {
         Connection conn = DBDataSource.getJDBCConnection();
         Statement stmt = null;
         ResultSet rs = null;
-        
-         ArrayList<Users> users = new  ArrayList<Users>();
+
+        ArrayList<Users> users = new ArrayList<Users>();
         try {
-             stmt = conn.createStatement();
+            stmt = conn.createStatement();
             String query = "SELECT Numero,Username,email,pwd,dateReceptionBon FROM Users ";
-            
-           
+
             rs = stmt.executeQuery(query);
-           
-            while(rs.next()){
+
+            while (rs.next()) {
                 Users u = new Users();
                 u.setId(rs.getLong("Numero"));
                 u.setUsername(rs.getString("Username"));
@@ -46,7 +49,7 @@ public class UsersDAO {
                 u.setDateReceptionBon(rs.getDate("dateReceptionBon"));
                 users.add(u);
             }
-            return users ;
+            return users;
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -56,10 +59,10 @@ public class UsersDAO {
                 conn.close();
             } catch (SQLException e) {
                 e.printStackTrace();
-                
+
             }
-            return users ;
-        } 
+            return users;
+        }
     }
 
     public Users select(String username) {
@@ -69,12 +72,12 @@ public class UsersDAO {
         Users u = new Users();
         try {
             String query = "SELECT * FROM Users WHERE Username = ?";
-            
-            stmt = conn.prepareStatement(query);        
+
+            stmt = conn.prepareStatement(query);
             stmt.setString(1, username);
             rs = stmt.executeQuery();
-           
-            while(rs.next()){
+
+            while (rs.next()) {
                 u.setId(rs.getLong("Numero"));
                 u.setUsername(rs.getString("Username"));
                 u.setEmail(rs.getString("email"));
@@ -88,15 +91,15 @@ public class UsersDAO {
                 rs.close();
                 stmt.close();
                 conn.close();
-                 return u;
+                return u;
             } catch (SQLException e) {
                 e.printStackTrace();
                 return null;
             }
-        } 
+        }
     }
 
-    public Long create(Long id, String username, String pwd, String email,Date dateReceptionBon) {
+    public Long create(Long id, String username, String pwd, String email, Date dateReceptionBon) {
         Connection conn = DBDataSource.getJDBCConnection();
         OraclePreparedStatement pstmt = null;
         ResultSet rs = null;
@@ -108,16 +111,16 @@ public class UsersDAO {
 
             pstmt = (OraclePreparedStatement) conn.prepareStatement(query); //create a statement
             pstmt.setString(1, username);
-            pstmt.setString(2,pwd);
-            pstmt.setString(3,email);
-            pstmt.setDate(4,dateReceptionBon);
+            pstmt.setString(2, pwd);
+            pstmt.setString(3, email);
+            pstmt.setDate(4, dateReceptionBon);
             pstmt.registerReturnParameter(5, OracleTypes.NUMBER);
 
             int count = pstmt.executeUpdate();
             conn.commit();
 
             if (count > 0) {
-               rs = pstmt.getReturnResultSet(); //rest is not null and not empty
+                rs = pstmt.getReturnResultSet(); //rest is not null and not empty
                 while (rs.next()) {
                     returnNumero = rs.getLong(1);
                     System.out.println(returnNumero);
@@ -138,125 +141,126 @@ public class UsersDAO {
             }
         }
     }
-    public void updateDate(Users user){
-       Connection conn = DBDataSource.getJDBCConnection();
-            PreparedStatement pstmt = null;
-            try {
-                
-                String query = "update USERS SET  DATERECEPTIONBON = SYSDATE WHERE numero = ?";
-                pstmt = (OraclePreparedStatement) conn.prepareStatement(query); //create a statement
-                 pstmt.setLong(1, user.getId());
-                 pstmt.executeUpdate();
+
+    public void updateDate(Users user) throws SQLException {
+        Connection conn = DBDataSource.getJDBCConnection();
+        PreparedStatement pstmt = null;
+        try {
+
+            String query = "update USERS SET  DATERECEPTIONBON = SYSDATE WHERE numero = ?";
+            pstmt = (OraclePreparedStatement) conn.prepareStatement(query); //create a statement
+            pstmt.setLong(1, user.getId());
+            pstmt.executeUpdate();
             conn.commit();
-             }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-                        }
-    
+        } finally {
+            pstmt.close();
+            conn.close();
+        }
+
     }
-    public Long updateProfil(Long id, String username, String pwd, String email, Date dateReceptionBon){
+
+    public Long updateProfil(Long id, String username, String pwd, String email, Date dateReceptionBon) {
         int executeUpdate = 0;
-            String query = null, endquery = null,susername = null, spwd = null, semail = null, sdateReceptionBon = null;
-            Connection conn = DBDataSource.getJDBCConnection();
-            Statement stmt = null;
-            try {
-                               
-                boolean onedone = false;
-                query = "update USERS SET ";
-                System.out.println("init query " + query);
-                endquery = " WHERE numero=" + id;
+        String query = null, endquery = null, susername = null, spwd = null, semail = null, sdateReceptionBon = null;
+        Connection conn = DBDataSource.getJDBCConnection();
+        Statement stmt = null;
+        try {
 
-                if (username != null) {
-                    susername = " USERNAME='" + username + "'";
-                }
-                System.out.println("spwd " + spwd);
-                if (pwd != null) {
-             
-                    spwd = " PWD='" + pwd + "'";
-                }
-                System.out.println("spwd " + spwd);
-                if (email != null) {
-                    
-                    semail = " EMAIL='" + email + "'";
-                }
-                if (dateReceptionBon != null) {
-                 
-                    sdateReceptionBon = " dateReceptionBon='" +dateReceptionBon + "'";
-                }
-                
-                 if (susername != null) {
-                    query = query.concat(susername);
-                    onedone = true;
-                }
-           System.out.println("avant pwd query " + query);
-                 if (spwd != null) {
-                    if (onedone) {
-                        query = query.concat(",");
-                    }
-                    query = query.concat(spwd);
-                    onedone = true;
-                }
-                 System.out.println("apres pwd query " + query);
-                 if (semail != null) {
-                    if (onedone) {
-                        query = query.concat(",");
-                    }
-                    query = query.concat(semail);
-                    onedone = true;
-                }
-                 
-                 if (sdateReceptionBon != null) {
-                    if (onedone) {
-                        query = query.concat(",");
-                    }
-                    query = query.concat(sdateReceptionBon);
-                    onedone = true;
-                }
-                
-                
-                
-           
-                query = query.concat(endquery);
+            boolean onedone = false;
+            query = "update USERS SET ";
+            System.out.println("init query " + query);
+            endquery = " WHERE numero=" + id;
 
-                System.out.println("updatequery ->" + query);
-
-                //create a statement
-                stmt = conn.createStatement();
-                executeUpdate = stmt.executeUpdate(query);
-                conn.commit();
-                System.out.println(executeUpdate + " Rows modified");
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    stmt.close();
-                    conn.close();
-                    return new Long(executeUpdate);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                    return new Long(executeUpdate);
-                }
+            if (username != null) {
+                susername = " USERNAME='" + username + "'";
             }
+            System.out.println("spwd " + spwd);
+            if (pwd != null) {
+
+                spwd = " PWD='" + pwd + "'";
+            }
+            System.out.println("spwd " + spwd);
+            if (email != null) {
+
+                semail = " EMAIL='" + email + "'";
+            }
+            if (dateReceptionBon != null) {
+
+                sdateReceptionBon = " dateReceptionBon='" + dateReceptionBon + "'";
+            }
+
+            if (susername != null) {
+                query = query.concat(susername);
+                onedone = true;
+            }
+            System.out.println("avant pwd query " + query);
+            if (spwd != null) {
+                if (onedone) {
+                    query = query.concat(",");
+                }
+                query = query.concat(spwd);
+                onedone = true;
+            }
+            System.out.println("apres pwd query " + query);
+            if (semail != null) {
+                if (onedone) {
+                    query = query.concat(",");
+                }
+                query = query.concat(semail);
+                onedone = true;
+            }
+
+            if (sdateReceptionBon != null) {
+                if (onedone) {
+                    query = query.concat(",");
+                }
+                query = query.concat(sdateReceptionBon);
+                onedone = true;
+            }
+
+            query = query.concat(endquery);
+
+            System.out.println("updatequery ->" + query);
+
+            //create a statement
+            stmt = conn.createStatement();
+            executeUpdate = stmt.executeUpdate(query);
+            conn.commit();
+            System.out.println(executeUpdate + " Rows modified");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stmt.close();
+                conn.close();
+                return new Long(executeUpdate);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return new Long(executeUpdate);
+            }
+        }
     }
 
     public Long delete(Long id) {
-        int executeUpdate=0;
+        int executeUpdate = 0;
 
         Connection conn = DBDataSource.getJDBCConnection();
         PreparedStatement pstmt = null;
 
         try {
-            String q="delete from Users where numero=?";
-     
+            String q = "delete from Users where numero=?";
+
             System.out.println("deletequery ->" + q);
 
             pstmt = conn.prepareStatement(q); //create a statement
-                //create a statement
-            pstmt.setLong(1,id);
+            //create a statement
+            pstmt.setLong(1, id);
             executeUpdate = pstmt.executeUpdate();
             conn.commit();
-            System.out.println( executeUpdate + " Rows modified" ) ;
-
+            System.out.println(executeUpdate + " Rows modified");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -267,7 +271,7 @@ public class UsersDAO {
                 return new Long(executeUpdate);
             } catch (SQLException e) {
                 e.printStackTrace();
-                return new Long (executeUpdate);
+                return new Long(executeUpdate);
             }
         }
     }
